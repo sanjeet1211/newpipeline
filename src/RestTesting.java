1e1ef1d1eed82f4535ced8025fbcef8f666511d7
sanjeet1211 <sanjeet.k.thakur@gmail.com>
2019-06-30 00:12:45 -0400

    push

--- src/RestTesting.java
+++ src/RestTesting.java
@@ -26,9 +26,6 @@ public class RestTesting {
 public void getUserData() {
    Response resp=RestAssured.get("http://samples.openweathermap.org/data/2.5/weather?q="+"Newyork"+","+"usa"+"&appid=b6907d289e10d714a6e88b30761fae22");
    System.out.println("response data"+resp.getStatusCode());
-   
-   RequestSpecification requestSpecification=RestAssured.given();
-   requestSpecification.header("content-type","application/json");
-JSONObject jsonobj=new JSONObject();
+
 }
 }
